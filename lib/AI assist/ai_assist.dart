e8bdcf623d774723c56f4dabee7384d44a289e4b
Dhruv Mehta <dhruvmehta634@gmail.com>
2025-02-08 12:24:13 +0530

    bottom

---
+++ lib/AI assist/ai_assist.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AiAssist extends StatefulWidget {
+  const AiAssist({super.key});
+
+  @override
+  State<AiAssist> createState() => _AiAssistState();
+}
+
+class _AiAssistState extends State<AiAssist> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
