24194fd3f33d901fc5e530695689f93cd6cda896
Dhruv Mehta <dhruvmehta634@gmail.com>
2025-02-08 13:02:42 +0530

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:amburush/bottomnav/bottom_navbar.dart';
+import 'package:amburush/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+   );
   runApp(const MyApp());
 }
 
